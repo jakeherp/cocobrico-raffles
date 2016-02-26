e8d73bbfb222850eabfc66bae697b1fcdf71db39
Jacob Herper <jacob@pcserve-media.com>
2016-02-26 15:17:00 +0000

    Admin Overviews

---
+++ resources/views/admin/create-raffle.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <section class="row" id="content">
+
+  	  <div class="large-12 column">
+        <h1><i class="fa fa-trophy"></i> Gewinnspiel hinzufügen</h1>
+        <div class="callout">
+		  {{FORMULAR}}
+        </div>
+      </div>
+
+    </section>
+    
+@endsection
