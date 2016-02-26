7a37043bb05cee80366fc8acb69343c082224541
Jacob Herper <jacob@pcserve-media.com>
2016-02-26 14:01:27 +0000

    Dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,9 +4,22 @@
     
     <section class="row" id="content">
 
-  	  <div class="large-12 column">
-
-
+  	  <div class="large-6 small-12 column">
+      	
+        <div class="callout">
+        	<h3>Gewinnspiel 1</h3>
+            Lorem ipsum bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+            <button class="alert button" disabled>Bereits angemeldet</button>
+        </div>
+          
+      </div>
+  	  <div class="large-6 small-12 column">
+      	
+        <div class="callout">
+        	<h3>Gewinnspiel 2</h3>
+            Lorem ipsum bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+            <button class="alert button">Teilnehmen</button>
+        </div>
           
       </div>
 
