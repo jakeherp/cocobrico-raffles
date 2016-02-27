2c419ebffe363df914cd5e08a500c2e9c06d1629
Jacob Herper <jacob@pcserve-media.com>
2016-02-27 13:19:26 +0000

    Impressum created

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,25 +4,29 @@
     
     <section class="row" id="content">
 
-        @foreach($raffles as $raffle)
-          <div class="large-6 small-12 column">
-            <div class="callout">
-              <h3>{{ $raffle->title }}</h3>
-              {{ $raffle->body }}
+      <div class="large-12 column">
+        <h1><i class="fa fa-trophy"></i> Verfügbare Gewinnspiele</h1>
+      </div>
 
-              {!! Form::open(['url' => 'dashboard', 'method' => 'post']) !!}
-                {!! Form::hidden('id', $raffle->id, []) !!}
-                <button class="alert button" 
-                @if($user->hasRaffle($raffle->id))
-                  disabled>Bereits angemeldet
-                @else
-                  role="submit">Teilnehmen
-                @endif
-                </button>
-              {!! Form::close() !!}
-            </div>
+      @foreach($raffles as $raffle)
+        <div class="large-6 small-12 column">
+          <div class="callout">
+            <h3>{{ $raffle->title }}</h3>
+            {{ $raffle->body }}
+
+            {!! Form::open(['url' => 'dashboard', 'method' => 'post']) !!}
+              {!! Form::hidden('id', $raffle->id, []) !!}
+              <button class="alert button" 
+              @if($user->hasRaffle($raffle->id))
+                disabled>Bereits angemeldet
+              @else
+                role="submit">Teilnehmen
+              @endif
+              </button>
+            {!! Form::close() !!}
           </div>
-        @endforeach
+        </div>
+      @endforeach
 
     </section>
     
