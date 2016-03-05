ca58c23ca3300ce36b073758a339764de708310c
Jacob Herper <jacob@pcserve-media.com>
2016-03-05 12:15:53 +0100

    Email Logo updated

--- resources/views/emails/confirmRaffle.blade.php
+++ resources/views/emails/confirmRaffle.blade.php
@@ -1,5 +1,5 @@
 <body style="background: #000;">
-	<img src="{{ URL::asset('img/logo.png') }}" alt="{{ trans('global.cocobrico') }}" style="width: 90%; max-width: 300px; display: block; margin: 30px auto;">
+	<img src="{{ URL::asset('img/logo_black.png') }}" alt="{{ trans('global.cocobrico') }}" style="width: 90%; max-width: 300px; display: block; margin: 30px auto;">
 	<div style="background: #fff; color: #000; padding: 20px; width: 90%; margin: 20px auto;">
 		<p>Hallo {{ $user->firstname }},</p>
 		<p>Vielen Dank für deine Anmeldung zu unserem {{ $raffle->title }} Gewinnspiel. Anbei findest du deine Bestätigung als PDF. Bringe diese bitte ausgedruckt mit, um zu erfahren, ob du gewonnen hast.<br><br>Viel Glück,<br>Dein Cocobrico Team</p>
--- resources/views/emails/verifyEmail.blade.php
+++ resources/views/emails/verifyEmail.blade.php
@@ -1,5 +1,5 @@
 <body style="background: #000;">
-	<img src="{{ URL::asset('img/logo.png') }}" alt="{{ trans('global.cocobrico') }}" style="width: 90%; max-width: 300px; display: block; margin: 30px auto;">
+	<img src="{{ URL::asset('img/logo_black.png') }}" alt="{{ trans('global.cocobrico') }}" style="width: 90%; max-width: 300px; display: block; margin: 30px auto;">
 	<div style="background: #fff; color: #000; padding: 20px; width: 90%; margin: 20px auto;">
 		<p>Hallo,</p>
 		<p>Bitte bestätige deine Email-Adresse, um die Registrierung deines Benutzerkontos fortzusetzen:</p>
