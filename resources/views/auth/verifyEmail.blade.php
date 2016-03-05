7fdeeae6170fb8fdb088067108bebb84ffc4edc4
Robert Schlick <robert.schlick@online.de>
2016-03-05 16:40:48 +0200

    Reend Verification Mail

--- resources/views/auth/verifyEmail.blade.php
+++ resources/views/auth/verifyEmail.blade.php
@@ -7,7 +7,11 @@
         <div class="callout large">
           <h3>Bitte bestätige deine Email-Adresse</h3>
           <div class="success callout">
-            <p>Wir haben eine Bestätigungsmail an deine Adresse {{ $user->email }} geschickt. Bitte klicke auf den darin enthaltenen Link, um deine Registrierung fortzusetzen.</p>
+            <p>Wir haben eine Bestätigungsmail an deine Adresse {{ $user->email }} geschickt. Bitte klicke auf den darin enthaltenen Link, um deine Registrierung fortzusetzen. </p>
+            {!! Form::open(['url' => 'email', 'method' => 'post']) !!}
+              {!! Form::hidden('user_id', $user->id) !!}
+              {!! Form::submit('Nochmal Senden', ['class' => 'button alert']) !!}
+            {!! Form::close() !!}
           </div>
         </div>
       </div>
