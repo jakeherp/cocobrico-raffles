34caea3af2629bb3427c378279b46d5ea55fb595
Robert Schlick <robert.schlick@online.de>
2016-03-09 17:34:32 +0200

    Raffles: Alternate Endings v1.1

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -44,18 +44,18 @@ class PagesController extends Controller
     	$id = $user->id;
     	$raffles_1 = $user->raffles()
     		->where('start','<=',time())
-    		->where('endState','=',0)
-    		->where('maxpState','=',0)
-    		->orWhere(function ($query) {
-	            $query->where('start','<=',time())
-	                  ->where('endState','=',1)
-	                  ->where('end','>',time());
-	        })
-	        ->orWhere(function ($query) {
-	            $query->where('start','<=',time())
-	                  ->where('maxpState','=',1)
-	                  ->where('maxpReached','=',0);
-	        })
+    		->where(function($q) {
+	    		$q->where('endState','=',0)
+	    		->where('maxpState','=',0)
+	    		->orWhere(function ($query) {
+		            $query->where('endState','=',1)
+		                  ->where('end','>',time());
+		        })
+		        ->orWhere(function ($query) {
+		            $query->where('maxpState','=',1)
+		                  ->where('maxpReached','=',0);
+		        });
+		       })
     		->orderBy('start', 'asc')->get();
 
     	$raffles_2 = Raffle::whereDoesntHave('users', function($q) use ($id){
