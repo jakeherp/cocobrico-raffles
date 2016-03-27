1e485fc4cdd9385bba548bf883583127f80821ed
Jacob Herper <jacob@pcserve-media.com>
2016-03-28 00:09:40 +0100

    [Version 0.15 f] Operator Templates

--- resources/views/operator/index.blade.php
+++ resources/views/operator/index.blade.php
@@ -5,7 +5,7 @@
 <section class="row" id="content">
   	<div class="large-12 column">
 	  	<h1><i class="fa fa-search"></i> Teilnehmersuche</h1>
-	  	<p>Geben Sie den Namen, den Geburtstag, den Reservierungscode oder den Gewinncode eines Benutzers ein!</p>
+	  	<p>Geben Sie den Namen, den Geburtstag, den Reservierungscode oder den Gewinncode eines Benutzers ein.</p>
 	  	@if(session()->has('msg'))
           <div class="callout {{ session('msgState') }}">
             <p>{{ session('msg') }}</p>
@@ -17,7 +17,7 @@
                     <span class="input-group-label"><i class="fa fa-user"></i></span>
                     {!! Form::text('search', null, ['class' => 'input-group-field', 'placeholder' => 'Suchbegriff']) !!}
                 </div>
-	    		{!! Form::submit('Suchen', ['class' => 'button alert large']) !!}
+	    		{!! Form::submit('Suchen', ['class' => 'large expanded alert button']) !!}
 	    	{!! Form::close() !!}
 	    </div>
   	</div>
--- resources/views/operator/user.blade.php
+++ resources/views/operator/user.blade.php
@@ -3,9 +3,36 @@
 @section('content')
 
 <section class="row" id="content">
-  	<div class="large-12 column">
-        {{ $member->firstname }} {{ $member->lastname }} {{ $member->email }}
-  	</div>
+	<div class="large-12 column">
+		<h1>{{ $member->firstname }} {{ $member->lastname }}</h1>
+
+		<div class="callout">
+			<div class="row">
+				<div class="medium-3 small-12 columns">
+					@if(($file = $member->files()->where('slug','profile_img')->first()) != null)
+						<img src="{{ URL::asset($file->path) }}">
+					@endif
+				</div>
+				<div class="medium-9 small-12 columns">
+					<h5>Zutritt (RAFFLE_TITLE)</h5>
+					Reservierungsnummer: <br>
+					Bestätigungscode: <br>
+					Teilnahmedatum: <br>
+					Zutritt gestattet: <strong><?=date("d.m.Y H:i:s")?></strong><br><br>
+
+					<h5>Persönliche Daten:</h5>
+					Email Adresse: {{ $member->email }}<br>
+					Anschrift: <br>
+					Mitglied seit:
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="small-12 column">
+		<button class="extralarge expanded alert button"><i class="fa fa-ban"></i><br>STOP</button>
+	</div>
+
 </section>
 
 @endsection
