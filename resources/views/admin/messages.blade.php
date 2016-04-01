33811a172128538b0a63910cbce905e14c6953e5
Jacob Herper <jacob@pcserve-media.com>
2016-04-02 00:44:54 +0200

    [Version 0.23 a] # Messages Template prepared for changes

--- resources/views/admin/messages.blade.php
+++ resources/views/admin/messages.blade.php
@@ -5,10 +5,13 @@
     <section class="row" id="content">
 
       <div class="large-12 column">
+        <button class="success button pull-right"><i class="fa fa-plus"></i> Neue Nachricht</button>
         <h1><i class="fa fa-envelope"></i> Nachrichten <div class="label">{{ count($conv1) }}</div></h1>
       </div>
 
       <div class="large-12 column">
+
+            <h4>Ungelesene Nachrichten</h4>
       	
       	<div class="callout">
       		@foreach($conv1 as $conv)
@@ -18,18 +21,18 @@
       			<a href="{{ URL('admin/messages/'.$conv->id) }}" class="divlink">
       				<div class="row">
 			      		<div class="medium-1 small-2 columns">
-			      			@if(($file = $conv->files()->where('slug','profile_img')->first()) != null)
-			                	<img src="{{ URL::asset($file->path) }}" style="border-radius: 50%; margin-right: 1rem;">
-			                @else
-			                	<img src="http://placehold.it/50x50" style="border-radius: 50%; margin-right: 1rem;">
-			                @endif
+                                        @if(($file = $conv->files()->where('slug','profile_img')->first()) != null)
+                                        <div class="round-image" style="background:url('{{ URL::asset($file->path) }}') no-repeat center center;background-size:cover;"></div>
+                                        @else
+                                          Kein Foto
+                                        @endif
 				      	</div>
 				      	<div class="medium-8 small-7 columns">
 				      		<strong>{{ $conv->firstname }} {{ $conv->lastname }}</strong>
 				      		<p>{{ substr($message->text,0,50) }} @if(strlen($message->text) > 50) ... @endif</p>
 				      	</div>
 				      	<div class="medium-3 small-3 columns text-right">
-				      		<em>{{ date(trans('global.datetimeformat'),$message->sent_at) }}</em>
+				      		<em>{{ date(trans('global.datetimeformat'),$message->sent_at) }}</em> <div class="ampel yellow"></div>
 				      	</div>
 			      </div>
       			</a>
@@ -39,35 +42,68 @@
 
       <div class="large-12 column">
 
-      	<h4>Beantwortete Nachrichten</h4>
-      	
-      	<div class="callout">
+            <h4>Unbeantwortete Nachrichten</h4>
+            
+            <div class="callout">
 
-      		@foreach($conv2 as $conv)
-      			<?php
-      				$message =  $conv->messages()->orderBy('sent_at', 'desc')->first();
-      			?>
-      			<a href="{{ URL('admin/messages/'.$conv->id) }}" class="divlink">
-      				<div class="row">
-			      		<div class="medium-1 small-2 columns">
-			      			@if(($file = $conv->files()->where('slug','profile_img')->first()) != null)
-			                	<img src="{{ URL::asset($file->path) }}" style="border-radius: 50%; margin-right: 1rem;">
-			                @else
-			                	<img src="http://placehold.it/50x50" style="border-radius: 50%; margin-right: 1rem;">
-			                @endif
-				      	</div>
-				      	<div class="medium-8 small-7 columns">
-				      		<strong>{{ $conv->firstname }} {{ $conv->lastname }}</strong>
-				      		<p>{{ substr($message->text,0,50) }} @if(strlen($message->text) > 50) ... @endif</p>
-				      	</div>
-				      	<div class="medium-3 small-3 columns text-right">
-				      		<em>{{ date(trans('global.datetimeformat'),$message->sent_at) }}</em>
-				      	</div>
-			      </div>
-      			</a>
-      		@endforeach
+                  @foreach($conv2 as $conv)
+                        <?php
+                              $message =  $conv->messages()->orderBy('sent_at', 'desc')->first();
+                        ?>
+                        <a href="{{ URL('admin/messages/'.$conv->id) }}" class="divlink">
+                              <div class="row">
+                                    <div class="medium-1 small-2 columns">
+                                          @if(($file = $conv->files()->where('slug','profile_img')->first()) != null)
+                                    <img src="{{ URL::asset($file->path) }}" style="border-radius: 50%; margin-right: 1rem;">
+                                  @else
+                                    <img src="http://placehold.it/50x50" style="border-radius: 50%; margin-right: 1rem;">
+                                  @endif
+                                    </div>
+                                    <div class="medium-8 small-7 columns">
+                                          <strong>{{ $conv->firstname }} {{ $conv->lastname }}</strong>
+                                          <p>{{ substr($message->text,0,50) }} @if(strlen($message->text) > 50) ... @endif</p>
+                                    </div>
+                                    <div class="medium-3 small-3 columns text-right">
+                                          <em>{{ date(trans('global.datetimeformat'),$message->sent_at) }}</em>
+                                    </div>
+                        </div>
+                        </a>
+                  @endforeach
 
-      	</div>
+            </div>
+      </div>
+
+      <div class="large-12 column">
+
+            <h4>Beantwortete Nachrichten</h4>
+            
+            <div class="callout">
+
+                  @foreach($conv2 as $conv)
+                        <?php
+                              $message =  $conv->messages()->orderBy('sent_at', 'desc')->first();
+                        ?>
+                        <a href="{{ URL('admin/messages/'.$conv->id) }}" class="divlink">
+                              <div class="row">
+                                    <div class="medium-1 small-2 columns">
+                                          @if(($file = $conv->files()->where('slug','profile_img')->first()) != null)
+                                    <img src="{{ URL::asset($file->path) }}" style="border-radius: 50%; margin-right: 1rem;">
+                                  @else
+                                    <img src="http://placehold.it/50x50" style="border-radius: 50%; margin-right: 1rem;">
+                                  @endif
+                                    </div>
+                                    <div class="medium-8 small-7 columns">
+                                          <strong>{{ $conv->firstname }} {{ $conv->lastname }}</strong>
+                                          <p>{{ substr($message->text,0,50) }} @if(strlen($message->text) > 50) ... @endif</p>
+                                    </div>
+                                    <div class="medium-3 small-3 columns text-right">
+                                          <em>{{ date(trans('global.datetimeformat'),$message->sent_at) }}</em>
+                                    </div>
+                        </div>
+                        </a>
+                  @endforeach
+
+            </div>
       </div>
 
     </section>
